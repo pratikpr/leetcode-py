508acc1122e99879a30eb995d1639746af201e49
pratikpr <89504885+prpr4667@users.noreply.github.com>
2021-10-11 07:57:26 -0600

    amazon 12

---
+++ medium/IntToRoman.py
@@ -0,0 +1,15 @@
+class IntToRoman:
+    def intToRoman(self, num: int) -> str:
+        romans = {1000: "M", 900: "CM", 500: "D", 400: "CD", 100: "C", 90: "XC", 50: "L", 40: "XL",
+                  10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"}
+        res, i = "", 0
+
+        for i in romans:
+            res += (num // i) * romans[i]
+            num %= i
+
+        return res
+
+obj = IntToRoman()
+num = 59
+print(obj.intToRoman(num))
---
+++ medium/MostWater.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class MostWater:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0; right = len(height)-1
+        maxArea = 0
+
+        while(left < right):
+            maxArea = max(maxArea, min(height[left], height[right]) * (right - left))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return maxArea
+
+obj = MostWater()
+heights = [1,1]
+print(obj.maxArea(heights))
