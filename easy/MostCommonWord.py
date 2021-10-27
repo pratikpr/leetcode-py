5b1fcf8903deb4e79e94b61d228f108a6eef830a
pratikpr <89504885+prpr4667@users.noreply.github.com>
2021-10-27 08:40:00 -0600

    amazon 819

---
+++ easy/MostCommonWord.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+from typing import List
+
+
+class MostCommonWord:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        banned_words = set(banned)
+        ans = ""
+        max_count = 0
+        word_count = defaultdict(int)
+        word_buffer = []
+
+        for p, char in enumerate(paragraph):
+            # 1). consume the characters in a word
+            if char.isalnum():
+                word_buffer.append(char.lower())
+                if p != len(paragraph) - 1:
+                    continue
+
+            # 2). at the end of one word or at the end of paragraph
+            if len(word_buffer) > 0:
+                word = "".join(word_buffer)
+                if word not in banned_words:
+                    word_count[word] += 1
+                    if word_count[word] > max_count:
+                        max_count = word_count[word]
+                        ans = word
+                # reset the buffer for the next word
+                word_buffer = []
+
+        return ans
+
+obj = MostCommonWord()
+para = "Bob hit a ball, the hit BALL flew far after it was hit."
+banned = ["hit"]
+print(obj.mostCommonWord(para, banned))
