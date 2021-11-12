592bfe0fa228e7460fec2638f2a50f81923fb7e7
pratikpr <89504885+prpr4667@users.noreply.github.com>
2021-11-12 08:59:01 -0700

    all 138

---
+++ medium/CopyListRandomPointer.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+class CopyListRandomPointer:
+    def __init__(self):
+        self.visited = {}
+
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if not head:
+            return head
+
+        old = head
+        new = Node(old.val, None, None)
+        self.visited[old] = new
+
+        while old != None:
+            new.random = self.getClonedCopy(old.random)
+            new.next = self.getClonedCopy(old.next)
+
+            old = old.next
+            new = new.next
+
+        return self.visited[head]
+
+    def getClonedCopy(self, node):
+        if node:
+            if node in self.visited:
+                return self.visited[node]
+            else:
+                self.visited[node] = Node(node.val, None, None)
+                return self.visited[node]
+        return None
