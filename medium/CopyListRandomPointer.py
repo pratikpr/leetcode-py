10347198d9806330c34c7b9f4a915f6e9d9585e0
pratikpr <89504885+prpr4667@users.noreply.github.com>
2021-11-12 09:02:02 -0700

    all 138

--- medium/CopyListRandomPointer.py
+++ medium/CopyListRandomPointer.py
@@ -1,3 +1,10 @@
+'''
+https://leetcode.com/problems/copy-list-with-random-pointer/
+A linked list of length n is given such that each node contains an additional random pointer,
+which could point to any node in the list, or null.
+
+Construct a deep copy of the list.
+'''
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
