4faae5d9dc4737d808b0940f7a7525d40ebe5b94
pratikpr <prpr4667@colorado.edu>
2022-03-17 16:38:30 -0600

    longest substring with distinct characters - alt

--- educative/sliding_window/LongestSubstringWithDistinctCharacters(hard).py
+++ educative/sliding_window/LongestSubstringWithDistinctCharacters(hard).py
@@ -6,21 +6,13 @@ def non_repeat_substring(str):
 	
 	while end < len(str):
 		last = str[end]
-		if last not in freqs:
-			freqs[last] = 0
-		freqs[last] += 1
-		
-		if freqs[last] > 1:
-			while len(freqs) < end-start+1:
-				first = str[start]
-				freqs[first] -= 1
-				if freqs[first] == 0:
-					del freqs[first]
-				start += 1
-		max_length = max(max_length, end - start + 1)
+		if last in freqs:
+			start = max(start, freqs[last] + 1)
+		freqs[last] = end
+		max_length = max(max_length, end-start+1)
 		end += 1
 	
 	return max_length
 		
-str = "aabccbb"
+str = "abccde"
 print(non_repeat_substring(str))
